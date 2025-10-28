22ef692d8ab3fb15756937333ff932027b9e34a8
KevX36 <kevincard2007@gmail.com>
2025-10-28 11:52:51 -0300

    set up finished

---
+++ Assets/list_of_sound_effects_used.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class list_of_sound_effects_used : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+// the exspolsion effect is from pixabay  made by sound reality, the wilhelm screem is from the same site by freesound_community
